0c1ffc22529c9303dd03f1f47b1db722ce5d514f
daanyaalkellerman <kellermandaanyaal2@gmail.com>
2024-03-05 16:21:02 +0200

    some responsiveness

--- src/App.vue
+++ src/App.vue
@@ -16,7 +16,9 @@ export default {
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Poppins&display=swap');
 *{
+    font-family: Poppins  ;
     margin: 0;
     padding: 0;
     box-sizing: border-box;
