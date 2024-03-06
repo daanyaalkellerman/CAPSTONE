d12b4b499540564a5b5e2bd7d6aaac59e7da3d64
daanyaalkellerman <kellermandaanyaal2@gmail.com>
2024-03-06 11:30:50 +0200

    added contact

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -1,13 +1,56 @@
 <template lang="">
-    <div>
+<body class="bodBack">
+    <section class="contactNexa">
         
-    </div>
+        <div class="contact">
+        <div class="info">
+            <h3>CONTACT US</h3>
+        </div>
+        <form class="form">
+                
+        </form>
+        </div>
+    </section>
+</body>
 </template>
 <script>
 export default {
     
 }
 </script>
-<style lang="">
+<style scoped>
+.contact{
+    display: flex;
+    justify-content: center;
+    width:900px;
+    height:60vh;
+}
+.info{
+    width:40%;
+    text-align:center;
+    background-color:#040B13
+}
+.form{
+    width:60%;
+}
+.contactNexa{
+    min-height: 100vh;
+    display: grid;
+    place-items: center; 
+    padding: 0;
+
+    background-image: url(https://i.postimg.cc/vBsW0q9g/Upscales-ai-1709458709523.webp);
+    background-size: cover;
+    background-position: center;
+ 
+  
+    text-align: center;
+    border-image: fill 1 linear-gradient(#040B13, rgba(0, 0, 0, 0.067));
     
+}
+.bodBack{
+    background-color: #040B13;
+    width: auto;
+    color: white;
+}
 </style>
