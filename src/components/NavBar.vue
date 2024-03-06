95919a0a11494c6acb401abc1ebcf0fbe5b90bd2
daanyaalkellerman <kellermandaanyaal2@gmail.com>
2024-03-06 08:50:32 +0200

    added profile css

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -14,6 +14,9 @@
         <div class="offcanvas-body">
           <ul class="navbar-nav justify-content-start flex-grow-1 pe-3">
             <li class="nav-item">
+                <router-link to="/profile" class="nav-link">Profile</router-link>
+              </li>
+            <li class="nav-item">
                 <router-link to="/login" class="nav-link">Login</router-link>
               </li>
             <li class="nav-item">
