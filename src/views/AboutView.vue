24291a896edae8ace96175e5a17be8878ae44f87
daanyaalkellerman <kellermandaanyaal2@gmail.com>
2024-03-06 12:07:10 +0200

    added more about

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -2,7 +2,9 @@
 <body class="bodBack">
     <section class="aboutNexa">
         <h1 class="aboutH">ABOUT US</h1>
-    <p class="aboutWords">Vivamus congue, diam sit amet bibendum pellentesque, arcu arcu mattis arcu, eu rhoncus dui ante vitae magna. Quisque feugiat velit lorem, non congue nibh rhoncus non. Duis a mauris massa. Praesent sit amet lacus eu lacus auctor dapibus eu nec massa. Sed id tellus vel elit pretium semper. Duis in lorem auctor, ultricies nisl nec, posuere lectus. Pellentesque hendrerit turpis a arcu porta, eget sollicitudin nisi cursus. Mauris vulputate neque tempor nunc auctor, sed lacinia lorem tincidunt. In urna dolor, dapibus eget tristique quis, interdum vitae quam. Maecenas tincidunt urna eu finibus tempus. Sed feugiat varius varius. Phasellus sed interdum magna, vel sodales nisl. Praesent ac purus arcu.</p>    
+    <p class="aboutWords">
+     Welcome to Drive Nexa, where we have redefined the car-buying experience to make quality, reliable vehicles more affordable for everyone. We prioritize offering a diverse range of car models that combine reliability and performance without breaking the bank. Our commitment to transparency means you can explore detailed information about each model, from specifications to maintenance history, empowering you to make informed decisions. With a focus on customer satisfaction, we've streamlined the purchasing process, ensuring a hassle-free experience. Whether you're a first-time buyer or looking to upgrade, our affordable options cater to various budgets, allowing you to drive away in a dependable vehicle that meets your needs. Experience the future of car buying, where affordability meets reliability, only at Drive Nexa.
+    </p>    
     </section>
 </body>
 </template>
@@ -16,6 +18,9 @@ export default {
     background-color: #040B13;
    width: auto;
    color: white;
+}
+.aboutWords{
+    font-size: 20px;
 }
     .aboutNexa{
 
@@ -32,5 +37,21 @@ export default {
     text-align: center;
     border-image: fill 1 linear-gradient(#040B13, rgba(0, 0, 0, 0.067));
     
+}
+@media (max-width:380px){
+    .aboutWords{
+        font-size:10px;
+    }
+    
+}
+@media (min-width:420px) and (max-width:786px){
+    .aboutWords{
+        font-size: 13px;
+    }
+}
+@media (min-width:800px) and (max-width:1030px){
+    .aboutWords{
+        font-size: 15px;
+    }
 }
 </style>
