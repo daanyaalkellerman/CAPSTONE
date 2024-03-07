9304c11ec040f4545c6d5f20f1054a816018d227
daanyaalkellerman <kellermandaanyaal2@gmail.com>
2024-03-07 12:34:14 +0200

    added more css

--- src/views/AdminView.vue
+++ src/views/AdminView.vue
@@ -2,7 +2,7 @@
     <body class="bodBack">
       <section class="adminNexa">
         <div>
-            <h2 class="tableTop">User Information</h2>
+            <h2 class="tableTop">Users</h2>
             <button type="button" class="btn btn-secondary my-2" data-bs-toggle="modal" data-bs-target="#exampleModal">Add User</button>
         </div>
         <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
@@ -79,6 +79,90 @@
               </div>
             </div>
           </div>
+          <!-- PRODUCT TABLE -->
+          <div>
+            <h2 class="tableTop">Products</h2>
+            <button type="button" class="btn btn-secondary my-2" data-bs-toggle="modal" data-bs-target="#exampleModal">Add Product</button>
+        </div>
+        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+              <div class="modal-content">
+                <div class="modal-header">
+                  <h5 class="modal-title" id="exampleModalLabel">Add Product</h5>
+                  <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close" @click="setTimeout()"></button>
+                </div>
+                <div class="modal-body">
+                    <!-- @submit.prevent="addUser" -->
+                  <form  method="POST" class="mx-4 my-5"> 
+                        <div class="row">
+                <div class="col">
+                    <p>Product Name</p>
+                  <input type="text" class="form-control" v-model="prodName" name="prodName"  aria-label="prodName" required="">
+                </div>
+              </div>
+                <div class="row">
+                  <div class="col">
+                      <p>Description</p>
+                    <input type="text" class="form-control" v-model="prodDes" name="prodDes" aria-label="prodDes" required="">
+                  </div>
+                </div>
+                <div class="row">
+                  <div class="col">
+                      <p>Image Url</p>
+                    <input type="text" class="form-control"  v-model="image" name="image" aria-label="image" required="">
+                  </div>
+                </div>
+              <div class="row">
+                <div class="col">
+                    <p>Category</p>
+                  <input type="text" class="form-control" v-model="category" name="category"  aria-label="Category" required="">
+                </div>
+              </div>
+              <div class="row">
+                <div class="col">
+                    <p>Price</p>
+                  <input type="text" class="form-control"  v-model="price" name="price" aria-label="Price" required="">
+                </div>
+              </div>
+              <div class="modal-footer">
+              <button type="submit" class="btn btn-dark"><span id="logs">Create product</span></button>
+            </div>
+        
+        </form>
+                </div> 
+            </div>
+            </div>
+            </div>
+          <div class="container">
+            <div class="row">
+              <div class="col">
+                <div class="table crud-table">
+                  <table class="table align-middle container-sm table-bordered">
+                    <thead class="thead">
+                      <tr>
+                        <th>Name</th>
+                        <th>Description</th>
+                        <th>Category</th>
+                        <th>Price</th>
+                        <th>Image</th>
+                        <th>Edit</th>
+                      </tr>
+                    </thead>
+                    <tbody class="">
+                      <tr>
+                        <td data-label="Name  ">Daanyaal</td>
+                        <td data-label="Description">Kellerman</td>
+                        <td data-label="Category">Hatchback</td>
+                        <td data-label="Price">R100000</td>
+                        <td data-label="Image"><img src="https://i.postimg.cc/66gMXvhq/tablet-2560x1700.jpg" alt=""></td>
+                        <td data-label="Edit"><button class="editBtn">Edit</button><button class="delBtn">Del</button></td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </div>
+              </div>
+            </div>
+          </div>
     </section>
 </body>
 </template>
@@ -135,7 +219,7 @@ img{
   }
   
   .modal-content{
-    background: black;
+    background: white;
   }
   .modal-body p{
     margin: 0;
