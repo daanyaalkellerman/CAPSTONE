a476753c9adaa0b5f2cc55e7d62f2969c79fbbcd
daanyaalkellerman <kellermandaanyaal2@gmail.com>
2024-03-11 09:36:33 +0200

    subBack

--- index.js
+++ index.js
@@ -14,12 +14,16 @@ config()
 const PORT = process.env.PORT
 const app = express()
 
-app.use(cors(
-    {
-        origin:'http://localhost:8080',
-        credentials:true
-    }
-))
+app.use(cors())
+app.use( (req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Credentials", "true");
+    res.header("Access-Control-Allow-Methods", "*");
+    res.header("Access-Control-Request-Methods", "*");
+    res.header("Access-Control-Allow-Headers", "*");
+    res.header("Access-Control-Expose-Headers", "Authorization");
+    next();
+});
 app.use(express.json())
 app.use(cookieParser())
 app.use(express.static('views'))
